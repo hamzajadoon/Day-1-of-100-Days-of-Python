28d69d3b18c53f50b4032bc50fcc1f23d591d735
hamzajadoon <46929195+hamzajadoon@users.noreply.github.com>
2022-04-08 01:41:58 +0000

    Just a little practice of swapping the variables to get   an opposite output

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-#Fix the code below 👇
+#This is my string concatination practice
 #print("Day 1 - String Manipulation")
 #print('String Concatenation is done with the "+" sign.')
 #print('e.g. print("Hello " + "world")')
 #print("New lines can be created with a backslash and n.")
 
-print("Your name has " + str(len(input("What is your name? "))) + " characters in it")
+#Here I have use len function with str to print the input length of name characters
+# print("Your name has " + str(len(input("What is your name? "))) + " characters in it")
+
+
+# The code below is swapping the VARIABLES ouput
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b
+b = c
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
